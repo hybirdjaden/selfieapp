88fe2b60760e4ab62dd710874a18894285674560
hybirdjaden <86745186+hybirdjaden@users.noreply.github.com>
2021-09-06 15:17:47 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -10,4 +10,20 @@ recognition.onresult = function (event) {
     console.log(event);
     var content = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = content;
+    speak();
 }
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+camera = document.getElementById("camera");
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
